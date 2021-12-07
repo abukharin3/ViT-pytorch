8d8d2f3637fa601f4c28986654235c06144c1bc1
Alexander Bukharin <59148829+abukharin3@users.noreply.github.com>
2021-12-07 16:51:03 -0500

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python3 train.py --name cifar10-100_500 --dataset cifar10 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz
+python3 train.py --name cifar10-100_500 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz
