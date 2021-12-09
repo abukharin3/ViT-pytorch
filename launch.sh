5586e53a447653d36337859e9ed46b459a568e57
Alexander Bukharin <abukharin@lawn-128-61-29-159.lawn.gatech.edu>
2021-12-09 17:09:47 -0500

    Fix parse bug

--- launch.sh
+++ launch.sh
@@ -12,4 +12,4 @@ python3 train.py --name cifar100_500 --dataset cifar100 --model_type ViT-B_16 --
 
 python3 train.py --name cifar100_500 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz \
 	--train_batch_size 512 --num_steps 20000 --seed 42 --initial_warmup 2000 --final_warmup 8000 --final_threshold 0.15 \
-	--move_prune True
+	--move_prune
