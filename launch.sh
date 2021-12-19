7b4cd9fcb880a48e589807a56981e2c62f728f02
Alexander Bukharin <abukharin@Alexanders-MacBook-Air.local>
2021-12-19 16:19:04 +0100

    Add local window

--- launch.sh
+++ launch.sh
@@ -2,10 +2,14 @@
 
 python3 train.py --name cifar100_500 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz \
 	--train_batch_size 512 --num_steps 20000 --seed 42 --initial_warmup 2000 --final_warmup 8000 --final_threshold 0.15 \
-	--beta3 0.75
+	--beta3 0.75 --local_window --deltaT 10
 
 python3 train.py --name cifar100_500 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz \
 	--train_batch_size 512 --num_steps 20000 --seed 42 --initial_warmup 2000 --final_warmup 8000 --final_threshold 0.15 \
+	--beta3 0.75 --local_window --deltaT 100
+
+python3 train.py --name cifar100_500 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz \
+	--train_batch_size 512 --num_steps 20000 --seed 42 --initial_warmup 2000 --final_warmup 8000 --final_threshold 0.2 \
 	--move_prune
 
 python3 train.py --name cifar100_500 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz \
