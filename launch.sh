77859043797067a6bc56d0dc4c704c01c3776967
Alexander Bukharin <abukharin@Alexanders-MacBook-Air.local>
2021-12-22 14:22:23 +0100

    Change launch file

--- launch.sh
+++ launch.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 python3 train.py --name cifar100_500 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz \
-	--train_batch_size 512 --num_steps 20000 --seed 42 --initial_warmup 2000 --final_warmup 8000 --final_threshold 0.3 \
+	--train_batch_size 512 --num_steps 20000 --seed 42 --initial_warmup 2000 --final_warmup 8000 --final_threshold 0.2 \
+	--beta3 0.75 --ma_uncertainty --ma_beta 0.85
 
 python3 train.py --name cifar100_500 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz \
 	--train_batch_size 512 --num_steps 20000 --seed 42 --initial_warmup 2000 --final_warmup 8000 --final_threshold 0.2 \
-	--ma_uncertainty --ma_beta 0.85
+	--beta3 0.75 --ma_uncertainty --ma_beta 0.5
+
+python3 train.py --name cifar100_500 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz \
+	--train_batch_size 512 --num_steps 20000 --seed 42 --initial_warmup 2000 --final_warmup 8000 --final_threshold 0.2 \
+	--beta3 0.75 --ma_uncertainty --ma_beta 0.95
 
 python3 train.py --name cifar100_500 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz \
 	--train_batch_size 512 --num_steps 20000 --seed 42 --initial_warmup 2000 --final_warmup 8000 --final_threshold 0.4 \
