a23f9d2421ceeeb7abe46062c32e12876276cbfb
Alexander Bukharin <abukharin@Alexanders-MacBook-Air.local>
2021-12-24 10:34:49 +0100

    Gpu

--- launch.sh
+++ launch.sh
@@ -2,25 +2,25 @@
 
 python3 train.py --name cifar100_500 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz \
 	--train_batch_size 512 --num_steps 20000 --seed 42 --initial_warmup 2000 --final_warmup 8000 --final_threshold 0.4 \
-	--beta3 0.75 --ma_uncertainty --ma_beta 0.85 --device_num 0
+	--beta3 0.75 --ma_uncertainty --ma_beta 0.85 --device_num 0,1,2,3
 
 python3 train.py --name cifar100_500 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz \
 	--train_batch_size 512 --num_steps 20000 --seed 42 --initial_warmup 2000 --final_warmup 8000 --final_threshold 0.5 \
-	--beta3 0.75 --ma_uncertainty --ma_beta 0.85 --device_num 0
+	--beta3 0.75 --ma_uncertainty --ma_beta 0.85 --device_num 0,1,2,3
 
 python3 train.py --name cifar100_500 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz \
 	--train_batch_size 512 --num_steps 20000 --seed 42 --initial_warmup 2000 --final_warmup 8000 --final_threshold 0.6 \
-	--beta3 0.75 --ma_uncertainty --ma_beta 0.85 --device_num 0
+	--beta3 0.75 --ma_uncertainty --ma_beta 0.85 --device_num 0,1,2,3
 
 python3 train.py --name cifar100_500 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz \
 	--train_batch_size 512 --num_steps 20000 --seed 42 --initial_warmup 2000 --final_warmup 8000 --final_threshold 0.7 \
-	--beta3 0.75 --ma_uncertainty --ma_beta 0.85 --device_num 0
+	--beta3 0.75 --ma_uncertainty --ma_beta 0.85 --device_num 0,1,2,3
 
 python3 train.py --name cifar100_500 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz \
 	--train_batch_size 512 --num_steps 20000 --seed 42 --initial_warmup 2000 --final_warmup 8000 --final_threshold 0.8 \
-	--beta3 0.75 --ma_uncertainty --ma_beta 0.85 --device_num 0
+	--beta3 0.75 --ma_uncertainty --ma_beta 0.85 --device_num 0,1,2,3
 
 python3 train.py --name cifar100_500 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz \
 	--train_batch_size 512 --num_steps 20000 --seed 42 --initial_warmup 2000 --final_warmup 8000 --final_threshold 0.9 \
-	--beta3 0.75 --ma_uncertainty --ma_beta 0.85 --device_num 0
+	--beta3 0.75 --ma_uncertainty --ma_beta 0.85 --device_num 0,1,2,3
 
--- launch2.sh
+++ launch2.sh
@@ -2,27 +2,27 @@
 
 python3 train.py --name cifar100_500 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz \
 	--train_batch_size 512 --num_steps 20000 --seed 42 --initial_warmup 2000 --final_warmup 8000 --final_threshold 0.4 \
-	--move_prune --device_num 0
+	--move_prune --device_num 4,5,6,7
 
 python3 train.py --name cifar100_500 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz \
 	--train_batch_size 512 --num_steps 20000 --seed 42 --initial_warmup 2000 --final_warmup 8000 --final_threshold 0.5 \
-	--move_prune --device_num 0
+	--move_prune --device_num 4,5,6,7
 
 python3 train.py --name cifar100_500 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz \
 	--train_batch_size 512 --num_steps 20000 --seed 42 --initial_warmup 2000 --final_warmup 8000 --final_threshold 0.6 \
-	--move_prune --device_num 0
+	--move_prune --device_num 4,5,6,7
 
 python3 train.py --name cifar100_500 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz \
 	--train_batch_size 512 --num_steps 20000 --seed 42 --initial_warmup 2000 --final_warmup 8000 --final_threshold 0.7 \
-	--move_prune --device_num 0
+	--move_prune --device_num 4,5,6,7
 
 python3 train.py --name cifar100_500 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz \
 	--train_batch_size 512 --num_steps 20000 --seed 42 --initial_warmup 2000 --final_warmup 8000 --final_threshold 0.8 \
-	--move_prune --device_num 0
+	--move_prune --device_num 4,5,6,7
 
 python3 train.py --name cifar100_500 --dataset cifar100 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz \
 	--train_batch_size 512 --num_steps 20000 --seed 42 --initial_warmup 2000 --final_warmup 8000 --final_threshold 0.9 \
-	--move_prune --device_num 0
+	--move_prune --device_num 4,5,6,7
 
 
 
